3373458cb3a48b49dfd76154aea848eb9729b154
ahyexng <kay561834@gmail.com>
2023-07-10 23:18:21 +0900

    setting: First setting

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# hatypo-studio-clone
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import GlobalStyle from "./styles/GlobalStyle";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
