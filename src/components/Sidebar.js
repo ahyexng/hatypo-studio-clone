9adc7a68119f95408d88b2bd821fbd1b3366dd24
ahyexng <kay561834@gmail.com>
2023-07-10 23:48:15 +0900

    feat/Sidebar add

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,35 @@
+import { styled } from "styled-components";
+import { FcInfo } from "react-icons/fc";
+
 const Sidebar = () => {
-  return <></>;
+  return (
+    <>
+      <Menu>
+        <Nav>
+          <NavList>
+            <FcInfo size="40" />
+          </NavList>
+          <NavList></NavList>
+          <NavList></NavList>
+          <NavList></NavList>
+          <NavList></NavList>
+          <NavList></NavList>
+          <NavList></NavList>
+        </Nav>
+      </Menu>
+    </>
+  );
 };
+
+const Menu = styled.div`
+  background-color: pink;
+  margin: 40px 500px;
+  height: 1000px;
+  width: 140px;
+  display: flex;
+  justify-content: center;
+`;
+
+const Nav = styled.div``;
+const NavList = styled.div``;
 export default Sidebar;
