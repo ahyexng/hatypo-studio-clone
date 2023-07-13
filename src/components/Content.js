3091c07cbb04f53d5a1794509d6966bb01f25339
ahyexng <kay561834@gmail.com>
2023-07-13 17:26:13 +0900

    feat/ Content 내용추가

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,8 +7,23 @@ import Seven from "../assets/images/seven.png";
 import Face from "../assets/icons/face.png";
 import More from "../assets/icons/more.png";
 import Call from "../assets/icons/call.png";
-
+import dummy from "../assets/data/msgListData.json";
 const Content = () => {
+  const monthNames = [
+    "Jan",
+    "Fab",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const date = new Date();
   return (
     <>
       <ContentWrap>
@@ -33,7 +48,58 @@ const Content = () => {
             <HeaderLastImg src={More} />
           </HeaderIconWrap>
         </ContentHeader>
-        <ContentBox></ContentBox>
+        <ContentBox>
+          <DateView>
+            <p>{`Today, ${monthNames[date.getMonth()]} ${date.getDay()}`}</p>
+          </DateView>
+          {dummy.Content.map((item, index) => {
+            return (
+              <ContentView>
+                {item.isMine ? (
+                  <MyMsg>
+                    <MsgHeader>
+                      <MyProfile src={item.profile} />
+                      <MyName>You</MyName>
+                      <MyMsgTime>{item.time}</MyMsgTime>
+                    </MsgHeader>
+                    {item.content &&
+                      item.content.map((data, index) => {
+                        return (
+                          <MyMsgBox>
+                            <MyMsgBoxP>{data}</MyMsgBoxP>
+                          </MyMsgBox>
+                        );
+                      })}
+                  </MyMsg>
+                ) : (
+                  <GroupMsg>
+                    <MsgHeader>
+                      <MsgProfile src={item.profile} />
+                      <MsgName>{item.name}</MsgName>
+                      <MsgTime>{item.time}</MsgTime>
+                    </MsgHeader>
+                    {item.content &&
+                      item.content.map((data, index) => {
+                        return (
+                          <MsgBox>
+                            <MsgBoxP>
+                              {data.Link ? (
+                                <a href="/">{data.Link}</a>
+                              ) : data.img ? (
+                                <img src={data.img} />
+                              ) : (
+                                data
+                              )}
+                            </MsgBoxP>
+                          </MsgBox>
+                        );
+                      })}
+                  </GroupMsg>
+                )}
+              </ContentView>
+            );
+          })}
+        </ContentBox>
         <ContentForm>
           <FormWrap>
             <ContentInput placeholder="Type a message"></ContentInput>
@@ -112,6 +178,123 @@ const ContentBox = styled.div`
   background-color: #f8f7fd;
   height: 600px;
   border-radius: 10px;
+  overflow: scroll;
+`;
+const DateView = styled.div`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  p {
+    width: 100px;
+    height: 30px;
+    background-color: white;
+    border-radius: 15px;
+    margin: 10px;
+    padding-top: 9px;
+    font-size: 13px;
+    font-weight: 600;
+  }
+`;
+const ContentView = styled.div`
+  display: flex;
+  width: 460px;
+
+  flex-direction: column;
+`;
+const GroupMsg = styled.div`
+  display: flex;
+  margin-left: 20px;
+  flex-direction: column;
+  margin-bottom: 10px;
+`;
+const MsgProfile = styled.img`
+  height: 40px;
+  width: 40px;
+  float: left;
+  background-color: pink;
+  border-radius: 25px;
+  padding: 3px;
+`;
+const MsgName = styled.p`
+  font-weight: 700;
+  float: left;
+  margin-left: 10px;
+  padding-top: 5px;
+`;
+const MsgTime = styled.p`
+  font-size: 13px;
+  color: gray;
+  padding-top: 7px;
+  width: 100px;
+  margin-left: 100px;
+`;
+const MsgBox = styled.div`
+  width: 400px;
+`;
+const MsgBoxP = styled.p`
+  margin-left: 40px;
+  text-align: left;
+  float: left;
+  padding: 10px;
+  margin-bottom: 10px;
+  font-size: 15px;
+  font-weight: 600;
+  background-color: white;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  border-top-right-radius: 10px;
+  img {
+    height: 40px;
+    width: 50px;
+  }
+  a {
+    color: blue;
+    text-decoration: underline;
+  }
+`;
+const MsgHeader = styled.div`
+  margin-right: 10px;
+`;
+const MyMsg = styled.div`
+  width: 460px;
+  display: flex;
+  flex-direction: column;
+`;
+const MyMsgTime = styled.p`
+  font-size: 13px;
+  color: gray;
+  padding-top: 16px;
+  width: 60px;
+  float: right;
+`;
+const MyName = styled.p`
+  font-weight: 700;
+  float: right;
+  margin: 0 12px 10px 20px;
+  padding-top: 10px;
+  font-size: 18px;
+`;
+const MyProfile = styled.img`
+  height: 40px;
+  width: 40px;
+  border-radius: 25px;
+  padding: 3px;
+  float: right;
+  margin: 10px 2px 5px 0;
+  background-color: pink;
+`;
+const MyMsgBox = styled.div`
+  margin: 0 44px 10px auto;
+`;
+const MyMsgBoxP = styled.p`
+  text-align: left;
+  padding: 13px 10px 10px 10px;
+  font-size: 15px;
+  font-weight: 600;
+  background-color: pink;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  border-top-left-radius: 10px;
 `;
 const ContentForm = styled.form`
   display: flex;
@@ -151,4 +334,5 @@ const ContentBtn = styled.button`
   margin-left: 10px;
   border-radius: 10px;
 `;
+
 export default Content;
