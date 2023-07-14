c3feb22066ffe18a0894555a5d27a5570092c5f8
ahyexng <kay561834@gmail.com>
2023-07-14 15:30:57 +0900

    refactor: Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -48,42 +48,42 @@ const NAV_ITEMS = [
   {
     id: 1,
     iconName: Clogo,
-    path: "/1",
+    path: "/",
   },
   {
     id: 2,
     iconName: Activity,
-    path: "/2",
+    path: "/",
   },
   {
     id: 3,
     iconName: Bubble,
-    path: "/main",
+    path: "/",
   },
   {
     id: 4,
     iconName: Call,
-    path: "/4",
+    path: "/",
   },
   {
     id: 5,
     iconName: Group,
-    path: "/5",
+    path: "/",
   },
   {
     id: 6,
     iconName: Grid,
-    path: "/6",
+    path: "/",
   },
   {
     id: 7,
     iconName: File,
-    path: "/7",
+    path: "/",
   },
   {
     id: 8,
     iconName: Setting,
-    path: "/8",
+    path: "/",
   },
 ];
 
@@ -120,7 +120,7 @@ const NavList = styled.li`
 const NavLast = styled.div`
   img {
     height: 40px;
-    background-color: white;
+    background-color: pink;
     border-radius: 25px;
   }
   margin-bottom: 30px;
